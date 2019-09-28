6cf6f7b16e80cf8287d6ba4469b4c2c09e2971cd
sebastienlebecq <slebecq@gmail.com>
2019-09-28 12:27:02 +0200

    correct commit de creation du composant bienvenue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BienvenueComponent } from './bienvenue/bienvenue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenueComponent
   ],
   imports: [
     BrowserModule,
